07072707e00be1b26730949f6314267be90fe4c0
Ali Ramazanov <aliasifzade@gmail.com>
2023-09-04 21:50:08 +0400

    Added Input Validation

--- script.js
+++ script.js
@@ -2,14 +2,63 @@
 
 const inputButton = document.querySelector(".input-button");
 
+const date = new Date();
+const thisYear = date.getFullYear();
+const toDay = date.getDate();
+const thisMonth = date.getMonth() + 1;
+
+const dayInput = document.getElementById("day-input");
+const dayError = document.getElementById("day-error");
+function validateDayInput() {
+  const dayValue = dayInput.value;
+
+  if (dayValue === "") {
+    dayError.textContent = "This field is required";
+  } else if (dayValue > 31 || dayValue < 1) {
+    dayError.textContent = "Must be a valid day";
+  } else {
+    dayError.textContent = "";
+  }
+}
+dayInput.addEventListener("input", validateDayInput);
+dayInput.addEventListener("blur", validateDayInput);
+
+const monthInput = document.getElementById("month-input");
+const monthError = document.getElementById("month-error");
+function validateMonthInput() {
+  const monthValue = monthInput.value;
+
+  if (monthValue === "") {
+    monthError.textContent = "This field is required";
+  } else if (monthValue > 12 || monthValue < 1) {
+    monthError.textContent = "Must be a valid month";
+  } else {
+    monthError.textContent = "";
+  }
+}
+monthInput.addEventListener("input", validateMonthInput);
+monthInput.addEventListener("blur", validateMonthInput);
+
+const yearInput = document.getElementById("year-input");
+const yearError = document.getElementById("year-error");
+function validateYearInput() {
+  const yearValue = yearInput.value;
+
+  if (yearValue === "") {
+    yearError.textContent = "This field is required";
+  } else if (yearValue > thisYear) {
+    yearError.textContent = "Must be in the past";
+  } else {
+    yearError.textContent = "";
+  }
+}
+yearInput.addEventListener("input", validateYearInput);
+yearInput.addEventListener("blur", validateYearInput);
+
 inputButton.addEventListener("click", () => {
   const dayInput = Number(document.getElementById("day-input").value);
   const monthInput = Number(document.getElementById("month-input").value);
   const yearInput = Number(document.getElementById("year-input").value);
-  const date = new Date();
-  const toDay = date.getDate();
-  const thisMonth = date.getMonth() + 1;
-  const thisYear = date.getFullYear();
 
   let yearsPassed = thisYear - yearInput;
   let yearsWord = "years";
